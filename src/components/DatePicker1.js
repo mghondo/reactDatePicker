f6448ff5235fd19482285dc50903f2e52a5213b3
Morgan Hondros <morganhondros@Morgans-MBP.localdomain>
2023-07-30 21:53:46 -0400

    DatePicker1 component working.

--- src/components/DatePicker1.js
+++ src/components/DatePicker1.js
@@ -1,18 +1,14 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 
+// basic Calendar Icon
 function DatePicker1() {
   const [startDate, setStartDate] = useState(new Date());
-  const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
-    <button className="example-custom-input" onClick={onClick} ref={ref}>
-      {value}
-    </button>
-  ));
   return (
     <DatePicker
+      showIcon
       selected={startDate}
       onChange={(date) => setStartDate(date)}
-      customInput={<ExampleCustomInput />}
     />
   );
 }
